0760ef424463857d8666586a505acdb96260bc8a
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2025-01-01 22:53:50 -0300

    feat: next and previous pagination functionality

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,23 +1,46 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Product, columns } from "./columns";
 import { DataTable } from "../../components/data-table";
-import { PaginationDemo } from "@/components/pagination";
+import { FullPagination } from "@/components/full-pagination";
+import { useSearchParams } from "next/navigation";
 
-async function getProducts(): Promise<Product[]> {
+async function getProducts({ page }: { page: number }): Promise<{
+  products: Product[];
+  pagination: { page: number; limit: number; total: number };
+}> {
   const data = await fetch(
-    "http://192.168.15.34:3000/products?limit=10&page=1",
+    `http://192.168.15.34:3000/products?limit=10&page=${page}`,
   );
-  const products = await data.json();
-  return products;
+  return await data.json();
 }
 
-export default async function Products() {
-  const data = await getProducts();
+export default function Products() {
+  const params = useSearchParams();
+  const [page, setPage] = useState(parseInt(params.get("page") || "1"));
+  const [totalPage, setTotalPage] = useState(0);
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    getProducts({ page }).then((data) => {
+      setProducts(data.products);
+      setTotalPage(data.pagination.total);
+    });
+  }, [page]);
+
+  const handleChangePage = (newPage: number) => setPage(newPage);
 
   return (
     <div className="container mx-auto flex flex-col gap-10">
       <h1 className="text-2xl font-bold">Produtos</h1>
-      <DataTable columns={columns} data={data} />
-      <PaginationDemo />
+      <DataTable columns={columns} data={products || []} />
+      <FullPagination
+        page={page}
+        pageSize={10}
+        totalCount={totalPage}
+        onChangePageAction={handleChangePage}
+      />
     </div>
   );
 }
