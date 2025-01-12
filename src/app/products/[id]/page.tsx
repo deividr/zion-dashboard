3801586f0dd3024b5d40d03c958a3f9f086fb417
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2025-01-11 22:47:35 -0300

    feat: update product in api

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,63 +3,186 @@
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Pencil } from "lucide-react";
-import { Trash2 } from "lucide-react";
+import { Pencil, Trash2, Loader2 } from "lucide-react";
 import { ArrowLeft } from "lucide-react";
 import { useParams, useRouter } from "next/navigation";
 import { Product } from "../columns";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  value: z.string(),
+  name: z.string().min(5, { message: "Nome deve ter no mínimo 5 caracteres" }),
+  unityType: z.enum(["UN", "KG"], {
+    message: "Tipo Unidade tem que ser UN ou KG",
+  }),
+});
+
+function formatCurrency(value: string) {
+  const numbers = value.replace(/\D/g, "");
+  const cents = Number(numbers) / 100;
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(cents);
+}
+
+function parseCurrency(value: string) {
+  return value.replace(/\D/g, "");
+}
 
 export default function ProductDetail() {
   const router = useRouter();
   const { id } = useParams();
   const [product, setProduct] = useState<Product | null>(null);
 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      value: "0.00",
+      name: "",
+      unityType: "KG",
+    },
+  });
+
   useEffect(() => {
     const fetchProduct = async () => {
       const result = await fetch(
-        `${process.env.NEXT_PUBLIC_HOST_API}/products/${id}`
+        `${process.env.NEXT_PUBLIC_HOST_API}/products/${id}`,
       );
       const data = await result.json();
+      form.reset({
+        value: String(data.value),
+        name: data.name,
+        unityType: data.unityType,
+      });
       setProduct(data);
     };
-
     fetchProduct();
-  }, [id]);
+  }, [id, form]);
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    const productUpdated: Product = {
+      id: product?.id || "",
+      value: parseInt(values.value),
+      name: values.name,
+      unityType: values.unityType,
+    };
+
+    const result = await fetch(
+      `${process.env.NEXT_PUBLIC_HOST_API}/products/${id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(productUpdated),
+      },
+    );
+
+    if (result.status !== 200) {
+      console.log("Erro ao atualizar o produto");
+    }
+
+    setProduct(productUpdated);
+  };
 
   if (!product) return <div>Loading...</div>;
 
   return (
-    <div className="container mx-auto flex flex-col gap-10">
-      <h1 className="text-2xl font-bold">
-        Produto <span className="text-orange-600">{product.name}</span>
-      </h1>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label>Nome</Label>
-        <Input value={product.name} disabled />
-      </div>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label>Tipo Unidade</Label>
-        <Input value={product.unityType} disabled size={10} />
-      </div>
-      <div className="grid w-full max-w-sm items-center gap-1.5">
-        <Label>Valor</Label>
-        <Input value={product.value} disabled />
-      </div>
-      <div className="flex gap-6">
-        <Button variant="outline" size="lg" onClick={() => router.back()}>
-          <ArrowLeft />
-          Voltar
-        </Button>
-        <Button variant="secondary" size="lg">
-          <Pencil />
-          Editar
-        </Button>
-        <Button variant="destructive" size="lg">
-          <Trash2 />
-          Excluir
-        </Button>
-      </div>
-    </div>
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="container mx-auto flex flex-col gap-5"
+      >
+        <h1 className="text-2xl font-bold">
+          Produto <span className="text-orange-600">{product.name}</span>
+        </h1>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nome</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <FormField
+            control={form.control}
+            name="value"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Valor</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    value={formatCurrency(field.value)}
+                    onChange={(e) => {
+                      const rawValue = parseCurrency(e.target.value);
+                      field.onChange(rawValue);
+                    }}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="grid w-full max-w-sm items-center gap-1.5">
+          <FormField
+            control={form.control}
+            name="unityType"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Tipo de Unidade</FormLabel>
+                <FormControl>
+                  <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className="flex gap-6 mt-10">
+          <Button
+            variant="outline"
+            type="button"
+            size="lg"
+            onClick={() => router.back()}
+          >
+            <ArrowLeft />
+            Voltar
+          </Button>
+          <Button
+            variant="secondary"
+            type="submit"
+            size="lg"
+            disabled={form.formState.isSubmitting}
+          >
+            {form.formState.isSubmitting ? (
+              <Loader2 className="animate-spin" />
+            ) : (
+              <Pencil />
+            )}
+            Savar
+          </Button>
+          <Button variant="destructive" type="button" size="lg">
+            <Trash2 />
+            Excluir
+          </Button>
+        </div>
+      </form>
+    </Form>
   );
 }
