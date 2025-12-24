211312974bd76f7229b256aade6e8a51238bbf15
Deivid Rodrigues <deivid.arodrigues@gmail.com>
2025-12-24 18:06:14 -0300

    feat: filter and pagination products without another fetc

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -15,87 +15,94 @@ import { columns } from "./columns";
 import { categoryEndpoints } from "@/repository/categoryRepository";
 
 export default function Products() {
-  const router = useRouter();
-  const params = useSearchParams();
-  const [isLoading, setLoading] = useState(true);
-  const [page, setPage] = useState(parseInt(params.get("page") || "1"));
-  const [totalPage, setTotalPage] = useState(0);
-  const [products, setProducts] = useState<Product[]>([]);
-  const [categories, setCategories] = useState<Category[]>([]);
-  const [search, setSearch] = useState(params.get("search") || "");
-  const { fetch } = useFetchClient();
-  const setTitle = useHeaderStore((state) => state.setTitle);
+    const PAGE_SIZE = 10;
+    const router = useRouter();
+    const params = useSearchParams();
+    const [isLoading, setLoading] = useState(true);
+    const [page, setPage] = useState(parseInt(params.get("page") || "1"));
+    const [totalProducts, setTotalProducts] = useState<number>(0);
+    const [allProducts, setAllProducts] = useState<Product[]>([]);
+    const [products, setProducts] = useState<Product[]>([]);
+    const [categories, setCategories] = useState<Category[]>([]);
+    const [search, setSearch] = useState(params.get("search") || "");
+    const { fetch } = useFetchClient();
+    const setTitle = useHeaderStore((state) => state.setTitle);
 
-  useEffect(() => {
-    setTitle(["Produtos"]);
-  }, [setTitle]);
+    useEffect(() => {
+        console.log("Effect 1");
+        setTitle(["Produtos"]);
+    }, [setTitle]);
 
-  useEffect(() => {
-    const fetchCategories = async () => {
-      const dataCategories = await fetch<Category[]>(categoryEndpoints.list());
-      setCategories(dataCategories || []);
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const dataCategories = await fetch<Category[]>(categoryEndpoints.list());
+            setCategories(dataCategories || []);
+        };
+        fetchCategories();
+    }, [setCategories, fetch]);
+
+    useEffect(() => {
+        console.log("Effect 2");
+        setLoading(true);
+        const fetchProducts = async () => {
+            const dataProducts = await fetch<{
+                products: Product[];
+            }>(productEndpoints.list(1, ""));
+            setAllProducts(dataProducts?.products || []);
+            setLoading(false);
+        };
+
+        fetchProducts();
+    }, [fetch]);
+
+    useEffect(() => {
+        console.log("Effect 3");
+        const startIndex = (page - 1) * PAGE_SIZE;
+        const endIndex = startIndex + PAGE_SIZE;
+        const filterProducts = allProducts.filter((p) => p.name.toLowerCase().includes(search.toLowerCase()));
+        setProducts(filterProducts.slice(startIndex, endIndex));
+        setTotalProducts(filterProducts.length);
+    }, [page, search, allProducts]);
+
+    const handleChangePage = (newPage: number) => setPage(newPage);
+
+    const buildLink = (newPage: number, newSearch: string) => {
+        const query = new URLSearchParams();
+        query.set("page", newPage.toString());
+
+        if (newSearch?.length) {
+            query.set("search", newSearch);
+        }
+
+        router.replace(`?${query.toString()}`);
     };
-    fetchCategories();
-  }, [setCategories, fetch]);
 
-  useEffect(() => {
-    setLoading(true);
-    const fetchProducts = async () => {
-      const dataProducts = await fetch<{
-        products: Product[];
-        pagination: { total: number };
-      }>(productEndpoints.list(page, search));
-      setProducts(dataProducts?.products || []);
-      setTotalPage(dataProducts?.pagination.total || 0);
-      setLoading(false);
-    };
-
-    fetchProducts();
-  }, [page, search, fetch]);
-
-  const handleChangePage = (newPage: number) => setPage(newPage);
-
-  const buildLink = (newPage: number, newSearch: string) => {
-    const query = new URLSearchParams();
-    query.set("page", newPage.toString());
-
-    if (newSearch?.length) {
-      query.set("search", newSearch);
-    }
-
-    router.replace(`?${query.toString()}`);
-  };
-
-  return (
-    <div className="flex flex-col gap-10">
-      <div className="flex gap-10">
-        <div className="grow">
-          <Input
-            placeholder="Pesquisar por produto..."
-            icon={Search}
-            value={search}
-            onChange={(e) => {
-              setPage(1);
-              setSearch(e.target.value);
-              buildLink(1, e.target.value);
-            }}
-          />
+    return (
+        <div className="flex flex-col gap-10">
+            <div className="flex gap-10">
+                <div className="grow">
+                    <Input
+                        placeholder="Pesquisar por produto..."
+                        icon={Search}
+                        value={search}
+                        onChange={(e) => {
+                            setPage(1);
+                            setSearch(e.target.value);
+                            buildLink(1, e.target.value);
+                        }}
+                    />
+                </div>
+                <Button onClick={() => router.push("products/new")}>
+                    <Plus /> Novo Produto
+                </Button>
+            </div>
+            <DataTable columns={columns(categories)} data={products} isLoading={isLoading} />
+            <FullPagination
+                page={page}
+                pageSize={PAGE_SIZE}
+                totalCount={totalProducts}
+                onChangePageAction={handleChangePage}
+            />
         </div>
-        <Button onClick={() => router.push("products/new")}>
-          <Plus /> Novo Produto
-        </Button>
-      </div>
-      <DataTable
-        columns={columns(categories)}
-        data={products}
-        isLoading={isLoading}
-      />
-      <FullPagination
-        page={page}
-        pageSize={10}
-        totalCount={totalPage}
-        onChangePageAction={handleChangePage}
-      />
-    </div>
-  );
+    );
 }
--- src/components/data-table.tsx
+++ src/components/data-table.tsx
@@ -1,100 +1,80 @@
 "use client";
 
-import {
-  ColumnDef,
-  flexRender,
-  getCoreRowModel,
-  useReactTable,
-} from "@tanstack/react-table";
-
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { ColumnDef, flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
 import { usePathname, useRouter } from "next/navigation";
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[];
-  data: TData[];
-  isLoading?: boolean;
+    columns: ColumnDef<TData, TValue>[];
+    data: TData[];
+    isLoading?: boolean;
 }
 
 interface DataRow {
-  id?: string;
+    id?: string;
 }
 
-export function DataTable<TData extends DataRow, TValue>({
-  columns,
-  data,
-  isLoading,
-}: DataTableProps<TData, TValue>) {
-  const table = useReactTable({
-    data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
-  const route = useRouter();
-  const pathname = usePathname();
+export function DataTable<TData extends DataRow, TValue>({ columns, data, isLoading }: DataTableProps<TData, TValue>) {
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    });
+    const route = useRouter();
+    const pathname = usePathname();
 
-  return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id} className="font-bold">
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {isLoading ? (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                <div className="flex items-center justify-center">
-                  <div className="h-6 w-6 animate-spin rounded-full border-2 border-primary border-t-transparent"></div>
-                  <span className="ml-2">Carregando...</span>
-                </div>
-              </TableCell>
-            </TableRow>
-          ) : table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-                onClick={() => route.push(`${pathname}/${row.original.id}`)}
-                className="cursor-pointer"
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  );
+    return (
+        <div className="rounded-md border">
+            <Table>
+                <TableHeader>
+                    {table.getHeaderGroups().map((headerGroup) => (
+                        <TableRow key={headerGroup.id}>
+                            {headerGroup.headers.map((header) => {
+                                return (
+                                    <TableHead key={header.id} className="font-bold">
+                                        {header.isPlaceholder
+                                            ? null
+                                            : flexRender(header.column.columnDef.header, header.getContext())}
+                                    </TableHead>
+                                );
+                            })}
+                        </TableRow>
+                    ))}
+                </TableHeader>
+                <TableBody>
+                    {isLoading ? (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                <div className="flex items-center justify-center">
+                                    <div className="h-6 w-6 animate-spin rounded-full border-2 border-primary border-t-transparent"></div>
+                                    <span className="ml-2">Carregando...</span>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ) : table.getRowModel().rows?.length ? (
+                        table.getRowModel().rows.map((row) => (
+                            <TableRow
+                                key={row.id}
+                                data-state={row.getIsSelected() && "selected"}
+                                onClick={() => route.push(`${pathname}/${row.original.id}`)}
+                                className="cursor-pointer"
+                            >
+                                {row.getVisibleCells().map((cell) => (
+                                    <TableCell key={cell.id}>
+                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))
+                    ) : (
+                        <TableRow>
+                            <TableCell colSpan={columns.length} className="h-24 text-center">
+                                No results.
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+        </div>
+    );
 }
